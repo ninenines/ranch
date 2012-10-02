e4496f05273d117001d6dc9c26413e4ba4335461
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-10-01 19:39:44 -0700

    Add expm package files

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(name: "ranch", description: "Socket acceptor pool for TCP protocols",
+                 version: "0.4.0", keywords: ["socket","TCP","acceptor","pool"], 
+                 licenses: [[name: "BSD", file: "LICENSE"]],
+                 maintainers: [[name: "Loïc Hoguin", 
+                                email: "essen@ninenines.eu"]],
+                 repositories: [[github: "extend/ranch", tag: "0.4.0"]])
+    
