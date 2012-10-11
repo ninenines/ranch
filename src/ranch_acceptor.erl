a66b105b49eb6668b1e5030977d74f1691c46b19
Andrew Majorov <encube.ul@gmail.com>
2012-10-11 18:35:48 +0400

    Allow acceptor to accept and handle sockets synchronously.
    Options {sync_accept, true} and {sync_handle, true} introduced through
    acceptor options list. These provide a way to accept or handle
    respectively in a synchronous manner, e.g. without spawning
    additional processes, which may in some cases give noticeable throughput
    gain.

--- src/ranch_acceptor.erl
+++ src/ranch_acceptor.erl
@@ -16,55 +16,90 @@
 -module(ranch_acceptor).
 
 %% API.
--export([start_link/6]).
+-export([start_link/7]).
 
 %% Internal.
--export([init/7]).
--export([loop/7]).
+-export([init/5]).
+-export([async_init/4]).
+-export([async_loop/4]).
+-export([sync_loop/4]).
 
 %% API.
 
--spec start_link(any(), inet:socket(), module(), module(), pid(), pid())
+-spec start_link(any(), inet:socket(), module(), module(), pid(), pid(), any())
 	-> {ok, pid()}.
-start_link(Ref, LSocket, Transport, Protocol, ListenerPid, ConnsSup) ->
+start_link(Ref, LSocket, Transport, Protocol, ListenerPid, ConnsSup, AccOpts) ->
 	{ok, MaxConns} = ranch_listener:get_max_connections(ListenerPid),
 	{ok, Opts} = ranch_listener:get_protocol_options(ListenerPid),
+	SyncAccept = lists:member({sync_accept, true}, AccOpts),
+	SyncHandle = lists:member({sync_handle, true}, AccOpts),
 	Pid = spawn_link(?MODULE, init,
-		[LSocket, Transport, Protocol, MaxConns, Opts, ListenerPid, ConnsSup]),
+		[LSocket, Transport, {Protocol, MaxConns, Opts, ListenerPid, ConnsSup}, SyncAccept, SyncHandle]),
 	ok = ranch_server:add_acceptor(Ref, Pid),
 	{ok, Pid}.
 
 %% Internal.
 
--spec init(inet:socket(), module(), module(),
-	non_neg_integer(), any(), pid(), pid()) -> no_return().
-init(LSocket, Transport, Protocol, MaxConns, Opts, ListenerPid, ConnsSup) ->
-	async_accept(LSocket, Transport),
-	loop(LSocket, Transport, Protocol, MaxConns, Opts, ListenerPid, ConnsSup).
+-spec init(inet:socket(), module(), tuple(), boolean(), boolean()) -> no_return().
+init(LSocket, Transport, Opts, false, SyncHandle) ->
+	async_init(LSocket, Transport, Opts, SyncHandle);
 
--spec loop(inet:socket(), module(), module(),
-	non_neg_integer(), any(), pid(), pid()) -> no_return().
-loop(LSocket, Transport, Protocol, MaxConns, Opts, ListenerPid, ConnsSup) ->
+init(LSocket, Transport, Opts, true, SyncHandle) ->
+	sync_loop(LSocket, Transport, Opts, SyncHandle).
+
+-spec async_init(inet:socket(), module(), tuple(), boolean()) -> no_return().
+async_init(LSocket, Transport, Opts, SyncHandle) ->
+	async_accept(LSocket, Transport),
+	async_loop(LSocket, Transport, Opts, SyncHandle).
+
+-spec async_loop(inet:socket(), module(), tuple(), boolean()) -> no_return().
+async_loop(LSocket, Transport, Opts = {Protocol, MaxConns, _, ListenerPid, ConnsSup}, SyncHandle) ->
 	receive
 		%% We couldn't accept the socket but it's safe to continue.
 		{accept, continue} ->
-			?MODULE:init(LSocket, Transport, Protocol,
-				MaxConns, Opts, ListenerPid, ConnsSup);
+			?MODULE:async_init(LSocket, Transport, Opts, SyncHandle);
 		%% Found my sockets!
 		{accept, CSocket} ->
-			{ok, ConnPid} = supervisor:start_child(ConnsSup,
-				[ListenerPid, CSocket, Transport, Protocol, Opts]),
-			Transport:controlling_process(CSocket, ConnPid),
-			ConnPid ! {shoot, ListenerPid},
-			NbConns = ranch_listener:add_connection(ListenerPid, ConnPid),
-			maybe_wait(ListenerPid, MaxConns, NbConns),
-			?MODULE:init(LSocket, Transport, Protocol,
-				MaxConns, Opts, ListenerPid, ConnsSup);
+			handle(Transport, CSocket, Opts, SyncHandle),
+			?MODULE:async_init(LSocket, Transport, Opts, SyncHandle);
 		%% Upgrade the protocol options.
 		{set_opts, Opts2} ->
-			?MODULE:loop(LSocket, Transport, Protocol,
-				MaxConns, Opts2, ListenerPid, ConnsSup)
+			?MODULE:async_loop(LSocket, Transport, {Protocol,
+					MaxConns, Opts2, ListenerPid, ConnsSup}, SyncHandle)
 	end.
+
+-spec sync_loop(inet:socket(), module(), tuple(), boolean()) -> no_return().
+sync_loop(LSocket, Transport, Opts = {Protocol, MaxConns, _, ListenerPid, ConnsSup}, SyncHandle) ->
+	receive
+		{set_opts, ProtoOpts2} ->
+			Opts2 = {Protocol, MaxConns, ProtoOpts2, ListenerPid, ConnsSup},
+			?MODULE:sync_loop(LSocket, Transport, Opts2, SyncHandle)
+	after 0 ->
+		ok
+	end,
+	case Transport:accept(LSocket, infinity) of
+		{ok, CSocket} ->
+			handle(Transport, CSocket, Opts, SyncHandle),
+			?MODULE:sync_loop(LSocket, Transport, Opts, SyncHandle);
+		%% We want to crash if the listening socket got closed.
+		{error, closed} ->
+			exit(closed);
+		{error, _Reason} ->
+			?MODULE:sync_loop(LSocket, Transport, Opts, SyncHandle)
+	end.
+
+handle(Transport, CSocket, {Protocol, MaxConns, Opts, ListenerPid, ConnsSup}, false) ->
+	{ok, ConnPid} = supervisor:start_child(ConnsSup,
+		[ListenerPid, CSocket, Transport, Protocol, Opts]),
+	Transport:controlling_process(CSocket, ConnPid),
+	ConnPid ! {shoot, ListenerPid},
+	NbConns = ranch_listener:add_connection(ListenerPid, ConnPid),
+	maybe_wait(ListenerPid, MaxConns, NbConns);
+
+handle(Transport, CSocket, {Protocol, _, Opts, ListenerPid, _}, true) ->
+	self() ! {shoot, ListenerPid},
+	%% let it fall awhile
+	Protocol:init(ListenerPid, CSocket, Transport, Opts).
 
 -spec maybe_wait(pid(), non_neg_integer(), non_neg_integer()) -> ok.
 maybe_wait(_, MaxConns, NbConns) when MaxConns > NbConns ->
