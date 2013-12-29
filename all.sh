723f1b1883e7834784eab772d74d9ede299f145b
Loïc Hoguin <essen@ninenines.eu>
2013-12-29 14:41:48 +0100

    Add a label to ct runs when testing across all Erlang releases

--- all.sh
+++ all.sh
@@ -13,7 +13,7 @@ do
 	. $KERL_INSTALL_PATH/$rel/activate
 	cp ~/.kerl/builds/$rel/otp_src_*/lib/ssl/test/erl_make_certs.erl \
 		deps/ct_helper/src/
-	make tests
+	CT_OPTS="-label $rel" make tests
 done
 
 xdg-open logs/all_runs.html
