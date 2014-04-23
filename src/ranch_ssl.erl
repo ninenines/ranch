ad2c080dc2825c9c705f8e542298253834abee1c
Loïc Hoguin <essen@ninenines.eu>
2014-04-23 11:40:40 +0200

    Ignore some errors that may occur during handshake
    These errors just pollute the logs when garbage is sent to the
    socket. Exit the process normally to avoid unwanted logs.

--- src/ranch_ssl.erl
+++ src/ranch_ssl.erl
@@ -96,6 +96,14 @@ accept_ack(CSocket, Timeout) ->
 	case ssl:ssl_accept(CSocket, Timeout) of
 		ok ->
 			ok;
+		%% Garbage was most likely sent to the socket, don't error out.
+		{error, {tls_alert, _}} ->
+			ok = close(CSocket),
+			exit(normal);
+		%% Socket most likely stopped responding, don't error out.
+		{error, timeout} ->
+			ok = close(CSocket),
+			exit(normal);
 		{error, Reason} ->
 			ok = close(CSocket),
 			error(Reason)
