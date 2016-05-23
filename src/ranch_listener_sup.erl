bd9bfdf42dd529072e56e94f8e0ca4315db7330a
ferigis <ferigis@gmail.com>
2016-05-23 10:48:48 -0700

    Renaming NbAcceptors to NumAcceptors

--- src/ranch_listener_sup.erl
+++ src/ranch_listener_sup.erl
@@ -20,14 +20,14 @@
 
 -spec start_link(ranch:ref(), non_neg_integer(), module(), any(), module(), any())
 	-> {ok, pid()}.
-start_link(Ref, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts) ->
+start_link(Ref, NumAcceptors, Transport, TransOpts, Protocol, ProtoOpts) ->
 	MaxConns = proplists:get_value(max_connections, TransOpts, 1024),
 	ranch_server:set_new_listener_opts(Ref, MaxConns, ProtoOpts),
 	supervisor:start_link(?MODULE, {
-		Ref, NbAcceptors, Transport, TransOpts, Protocol
+		Ref, NumAcceptors, Transport, TransOpts, Protocol
 	}).
 
-init({Ref, NbAcceptors, Transport, TransOpts, Protocol}) ->
+init({Ref, NumAcceptors, Transport, TransOpts, Protocol}) ->
 	AckTimeout = proplists:get_value(ack_timeout, TransOpts, 5000),
 	ConnType = proplists:get_value(connection_type, TransOpts, worker),
 	Shutdown = proplists:get_value(shutdown, TransOpts, 5000),
@@ -36,7 +36,7 @@ init({Ref, NbAcceptors, Transport, TransOpts, Protocol}) ->
 				[Ref, ConnType, Shutdown, Transport, AckTimeout, Protocol]},
 			permanent, infinity, supervisor, [ranch_conns_sup]},
 		{ranch_acceptors_sup, {ranch_acceptors_sup, start_link,
-				[Ref, NbAcceptors, Transport, TransOpts]},
+				[Ref, NumAcceptors, Transport, TransOpts]},
 			permanent, infinity, supervisor, [ranch_acceptors_sup]}
 	],
 	{ok, {{rest_for_one, 1, 5}, ChildSpecs}}.
