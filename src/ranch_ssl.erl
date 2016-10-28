ef19f828655c075661ee5211cbae470dc6f0166a
Alexandru Munteanu <munteanu@ebu.ch>
2016-10-28 04:45:01 +0200

    adding more SSL configurable otions

--- src/ranch_ssl.erl
+++ src/ranch_ssl.erl
@@ -36,7 +36,9 @@
 -export([shutdown/2]).
 -export([close/1]).
 
--type ssl_opt() :: {alpn_preferred_protocols, [binary()]}
+-type ssl_opt() :: {alpn_advertised_protocols, [binary()]}
+	| {alpn_preferred_protocols, [binary()]}
+	| {beast_mitigation, one_n_minus_one | zero_n | disabled}
 	| {cacertfile, string()}
 	| {cacerts, [public_key:der_encoded()]}
 	| {cert, public_key:der_encoded()}
@@ -48,6 +50,7 @@
 	| {depth, 0..255}
 	| {dh, public_key:der_encoded()}
 	| {dhfile, string()}
+	| {fallback, boolean()}
 	| {fail_if_no_peer_cert, boolean()}
 	| {hibernate_after, integer() | undefined}
 	| {honor_cipher_order, boolean()}
@@ -55,16 +58,20 @@
 	| {keyfile, string()}
 	| {log_alert, boolean()}
 	| {next_protocols_advertised, [binary()]}
+	| {padding_check, boolean()}
 	| {partial_chain, fun(([public_key:der_encoded()]) -> {trusted_ca, public_key:der_encoded()} | unknown_ca)}
 	| {password, string()}
 	| {psk_identity, string()}
 	| {reuse_session, fun()}
 	| {reuse_sessions, boolean()}
 	| {secure_renegotiate, boolean()}
+	| {server_name_indication, string()}
 	| {signature_algs, [{atom(), atom()}]}
 	| {sni_fun, fun()}
 	| {sni_hosts, [{string(), ssl_opt()}]}
+	| {srp_identity, [{Username :: string(), Password :: string()}]}
 	| {user_lookup_fun, {fun(), any()}}
+	| {v2_hello_compatible, boolean()}
 	| {verify, ssl:verify_type()}
 	| {verify_fun, {fun(), any()}}
 	| {versions, [atom()]}.
@@ -101,12 +108,14 @@ listen(Opts) ->
 			{reuseaddr, true}, {nodelay, true}])).
 
 listen_options() ->
-	[alpn_preferred_protocols, cacertfile, cacerts, cert, certfile,
-		ciphers, client_renegotiation, crl_cache, crl_check, depth,
-		dh, dhfile, fail_if_no_peer_cert, hibernate_after, honor_cipher_order,
-		key, keyfile, log_alert, next_protocols_advertised, partial_chain,
-		password, psk_identity, reuse_session, reuse_sessions, secure_renegotiate,
-		signature_algs, sni_fun, sni_hosts, user_lookup_fun, verify, verify_fun, versions
+	[alpn_advertised_protocols, alpn_preferred_protocols, beast_mitigation,
+		cacertfile, cacerts, cert, certfile, ciphers, client_renegotiation,
+		crl_cache, crl_check, depth, dh, dhfile, fallback, fail_if_no_peer_cert,
+		hibernate_after, honor_cipher_order, key, keyfile, log_alert,
+		next_protocols_advertised, partial_chain, password, padding_check,
+		psk_identity, reuse_session, reuse_sessions, secure_renegotiate,
+		server_name_indication, signature_algs, sni_fun, sni_hosts, srp_identity,
+		user_lookup_fun, v2_hello_compatible, verify, verify_fun, versions
 		|ranch_tcp:listen_options()].
 
 -spec accept(ssl:sslsocket(), timeout())
