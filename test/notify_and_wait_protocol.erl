8436e96544d79a5b5d32221925f60334cc33b205
juhlig <j.uhlig@mailingwork.de>
2019-06-03 09:47:31 +0200

    Fix many conns_sups tests for Windows

--- test/notify_and_wait_protocol.erl
+++ test/notify_and_wait_protocol.erl
@@ -2,12 +2,13 @@
 -behaviour(ranch_protocol).
 
 -export([start_link/3]).
--export([init/2]).
+-export([init/3]).
 
-start_link(_, _, [{msg, Msg}, {pid, TestPid}]) ->
-	Pid = spawn_link(?MODULE, init, [Msg, TestPid]),
+start_link(_, _, Opts = #{msg := Msg, pid := TestPid}) ->
+	Timeout = maps:get(timeout, Opts, 2500),
+	Pid = spawn_link(?MODULE, init, [Msg, TestPid, Timeout]),
 	{ok, Pid}.
 
-init(Msg, Pid) ->
+init(Msg, Pid, Timeout) ->
 	Pid ! Msg,
-	receive after 2500 -> ok end.
+	receive after Timeout -> ok end.
