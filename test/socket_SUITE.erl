51baf7a6b7d89addc25e9d4ea5d14fc94e0abdae
juhlig <j.uhlig@mailingwork.de>
2020-06-25 09:19:17 +0200

    test 6

--- test/socket_SUITE.erl
+++ test/socket_SUITE.erl
@@ -10,18 +10,19 @@ all() ->
 socket_test(_) ->
 	_ = process_flag(trap_exit, true),
 	{ok, S} = gen_tcp:listen(0, [{inet_backend, socket}, {packet, raw}, binary, {active, false}]),
-	{ok, {_, Port}} = inet:sockname(S),
-	P = spawn_link(
-		fun () ->
-			{ok, C} = gen_tcp:connect("localhost", Port, [{packet, raw}, binary, {active, false}]),
-			ok = gen_tcp:send(C, <<"foo">>),
-			{ok, <<"bar">>} = gen_tcp:recv(C, 0, 1000),
-			gen_tcp:close(C)
-		end
-	),
-	{ok, C} = gen_tcp:accept(S),
-	{ok, <<"foo">>} = gen_tcp:recv(C, 0, 1000),
-	ok = gen_tcp:send(C, <<"bar">>),
-	ok = gen_tcp:close(C),
-	ok = gen_tcp:close(S),
+%	{ok, {_, Port}} = inet:sockname(S),
+%	spawn_link(
+%		fun () ->
+%			{ok, C} = gen_tcp:connect("localhost", Port, [{packet, raw}, binary, {active, false}]),
+%			ok = gen_tcp:send(C, <<"foo">>),
+%			{ok, <<"bar">>} = gen_tcp:recv(C, 0, 1000),
+%			gen_tcp:close(C)
+%		end
+%	),
+%	{ok, C} = gen_tcp:accept(S),
+	{error, timeout} = gen_tcp:accept(S, 1000),
+%	{ok, <<"foo">>} = gen_tcp:recv(C, 0, 1000),
+%	ok = gen_tcp:send(C, <<"bar">>),
+%	ok = gen_tcp:close(C),
+%	ok = gen_tcp:close(S),
 	ok.
