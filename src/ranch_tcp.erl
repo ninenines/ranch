b97f90eb3be8a2c1030e50fe7a9d48612a765e25
juhlig <j.uhlig@mailingwork.de>
2020-06-25 12:05:45 +0200

    Update copyright

--- src/ranch_tcp.erl
+++ src/ranch_tcp.erl
@@ -1,4 +1,5 @@
-%% Copyright (c) 2011-2018, Loïc Hoguin <essen@ninenines.eu>
+%% Copyright (c) 2011-2020, Loïc Hoguin <essen@ninenines.eu>
+%% Copyright (c) 2020, Jan Uhlig <j.uhlig@mailingwork.de>
 %%
 %% Permission to use, copy, modify, and/or distribute this software for any
 %% purpose with or without fee is hereby granted, provided that the above
