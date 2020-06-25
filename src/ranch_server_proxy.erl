cca66e03595bbd315e48721ff7d2f3dcad1c488a
juhlig <j.uhlig@mailingwork.de>
2020-06-25 12:05:45 +0200

    Update copyright

--- src/ranch_server_proxy.erl
+++ src/ranch_server_proxy.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2019, Jan Uhlig <ju@mailingwork.de>
+%% Copyright (c) 2019-2020, Jan Uhlig <ju@mailingwork.de>
 %%
 %% Permission to use, copy, modify, and/or distribute this software for any
 %% purpose with or without fee is hereby granted, provided that the above
