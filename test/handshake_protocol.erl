0781ab631e4d6ea46b4947aeda36a23d94fb89ff
Loïc Hoguin <essen@ninenines.eu>
2026-09-03 15:09:28 +0200

    Drop OTP-22 SNI workaround in handshake_protocol

--- test/handshake_protocol.erl
+++ test/handshake_protocol.erl
@@ -9,15 +9,7 @@ start_link(Ref, Transport, Opts) ->
 	{ok, Pid}.
 
 init(Ref, Transport, Opts) ->
-	SniHost = case ranch:handshake(Ref) of
-		%% Due to a bug in ssl (https://bugs.erlang.org/browse/ERL-951,
-		%% fixed in OTP 22.0.3) the value for sni may be {sni, Hostname}
-		%% instead of Hostname.
-		{continue, #{sni := {sni, Hostname}}} ->
-			Hostname;
-		{continue, #{sni := Hostname}} ->
-			Hostname
-	end,
+	{continue, #{sni := SniHost}} = ranch:handshake(Ref),
 	SniHostOpts = maps:get(SniHost, Opts),
 	{ok, Socket} = ranch:handshake_continue(Ref, SniHostOpts),
 	loop(Socket, Transport).
